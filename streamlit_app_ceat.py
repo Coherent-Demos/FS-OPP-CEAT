b3dd817e44fe325e1f16f4aee800c2635472a7be
markbanaria <markbanaria@gmail.com>
2024-01-17 12:24:37 +0800

    updates ui

--- streamlit_app_ceat.py
+++ streamlit_app_ceat.py
@@ -42,6 +42,12 @@ def highlight_columns(s, columns):
 
     return styles
 
+if 'active_expander' not in st.session_state:
+    st.session_state.active_expander = None
+
+def set_active_expander(expander_name):
+  st.session_state.active_expander = expander_name
+
 inputdata = {"EXEC_1": {}, "ALL_EXECS":{}}
 SparkErrors = []
 
@@ -49,37 +55,35 @@ SparkErrors = []
 image_path = "ceat-logo.png"
 st.image(image_path, caption="", width=120)
 
-st.write("## Incentive Calculation")
+st.write("## CEAT Sales Incentive Program")
 
 col21, col22, col23 = st.columns([12, 2, 32])
 
 with col21:
   st.text("‎") 
-  st.write("Inputs")
+  st.write("**Inputs**")
   with st.form("DC Form"):
 
-    with st.expander("**Files**"):
+    with st.expander("**Spark Services**"):
       inputdata["Service_Actuals_Data"] = st.text_input("Actuals Data", "2024Q1 - Actual Sales Performance")
       inputdata["Service_Budget_Data"] = st.text_input("Budget Data", "2022-24 - CEAT Budget")
       inputdata["Service_Calc_Model"] = st.text_input("Calc Model", "Incentive Calculator")
       inputdata["Service_KPIs_and_Criteria"] = st.text_input("KPIs and Criteria", "2023 - KPIs & Incentive Criteria")
-    
-    with st.expander("**Employee**"):
-      inputdata["Exec1_Employee_ID"] = st.text_input("Enter Employee ID", 636718)
 
-    with st.expander("**Evaluation Period**"):
+    with st.expander("**Budget Period**"):
       inputdata["Quarter"] = st.text_input("Quarter", "Q2")
       inputdata["Year"] = st.text_input("Year", 2022)
-
-    with st.expander("**Set Filename for Reports**"):
-      inputdata["EXEC_1"]["ReportName"] = st.text_input("Individual Report", "EXEC_1")
-      inputdata["ALL_EXECS"]["ReportName"] = st.text_input("Group Report", "ALL_EXECS")
+    
+    with st.expander("**Sales Executive**"):
+      inputdata["Exec1_Employee_ID"] = st.text_input("Enter Employee ID", 636718)
 
     with st.expander("**API Key**"):
-      bearerToken = st.text_input("API Token", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJwaUppeFpjYTJEZzVBZ0FneUZxUzU5d0RKV09lMC03VENBbUpZWktFTXRrIn0.eyJleHAiOjE3MDU0MDEzMTUsImlhdCI6MTcwNTM5NDExNSwiYXV0aF90aW1lIjoxNzA1Mzk0MTE0LCJqdGkiOiI2NzgzNjc2ZS1kYTUwLTQ3OWEtYWNhZS1kYjJmNTAzOGVmZGMiLCJpc3MiOiJodHRwczovL2tleWNsb2FrLnVhdC5qcC5jb2hlcmVudC5nbG9iYWwvYXV0aC9yZWFsbXMvbnR0ZGF0YSIsImF1ZCI6InByb2R1Y3QtZmFjdG9yeSIsInN1YiI6ImNlMWNlYzgxLWFjZDQtNGMwYi05NDNmLWRjMTE5Mzg2MTRiZiIsInR5cCI6IkJlYXJlciIsImF6cCI6InByb2R1Y3QtZmFjdG9yeSIsIm5vbmNlIjoiNjhhNDQ2ODEtMjBlMi00YzI1LWFlNGQtMGEwMjY3YjkyYmMwIiwic2Vzc2lvbl9zdGF0ZSI6IjVmMTkxNjZiLTI0MjQtNGZmZi1hM2U4LTQzZDcyYjlmMTczYyIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cHM6Ly9tb2RlbGluZy1jZW50ZXIuZGV2LmNvaGVyZW50Lmdsb2JhbCIsImh0dHBzOi8vbW9kZWxpbmctY2VudGVyLnVzLmNvaGVyZW50Lmdsb2JhbCIsImh0dHBzOi8vc2EudWF0LmpwLmNvaGVyZW50Lmdsb2JhbCIsImh0dHBzOi8vc2Euc3RhZ2luZy5jb2hlcmVudC5nbG9iYWwiLCJodHRwczovL3Byb2R1Y3RmYWN0b3J5LnVhdC5qcC5jb2hlcmVudC5nbG9iYWwiLCJodHRwczovL3NwYXJrLnVhdC5qcC5jb2hlcmVudC5nbG9iYWwiLCJodHRwczovL3NwYXJrLXVzZXItbWFuYWdlci51YXQuanAuY29oZXJlbnQuZ2xvYmFsIiwiaHR0cHM6Ly9tb2RlbGluZy1jZW50ZXIuc3RhZ2luZy5jb2hlcmVudC5nbG9iYWwiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIl19LCJzY29wZSI6Im9wZW5pZCBvZmZsaW5lX2FjY2VzcyBzcGFyayBwcm9maWxlIiwic2lkIjoiNWYxOTE2NmItMjQyNC00ZmZmLWEzZTgtNDNkNzJiOWYxNzNjIiwibmFtZSI6Ik1hcmsgQmFuYXJpYSIsImdyb3VwcyI6WyJ1c2VyOnBmIl0sInJlYWxtIjoibnR0ZGF0YSIsInByZWZlcnJlZF91c2VybmFtZSI6Im1hcmsuYmFuYXJpYUBjb2hlcmVudC5nbG9iYWwiLCJnaXZlbl9uYW1lIjoiTWFyayIsImZhbWlseV9uYW1lIjoiQmFuYXJpYSIsInVzZXJfY3JlYXRlZF90aW1lc3RhbXAiOjE3MDUwNTI5NzkzNTMsInJlbGF0aW9uIjoicGFydG5lciJ9.eLRV_GLn27jLyrMbPnlo9SGzBJduS95q25qHi-Ncb946Zf6BQSN3gXxJFQ0KVg0TPSNVpAfEUDPckQrYgg2z_tbKIbXalk2wNWe7HrRtzDQQnHrvWlg39il9ZMj4m-HTnEfQ_2aZtTW4B3RKYu-0ytYSiYzOokRoAML5gQwJEK-CqQtW3FAV8XShsJ_YPAs9dsOQua97hXScIRLUa-DAuk5iGtlHIYxifRCkNRvxPyU6rGENCqXrzAyHftOuD6ltgO63g_V5DBDzJSHsdA0eB6QG5dNhn6WTvVYXF9YKIU-h0O30yfrekmtc1wBdhwdotGzhLig1eGuO5d5fKgIeBA")
+      bearerToken = st.text_input("API Token", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJwaUppeFpjYTJEZzVBZ0FneUZxUzU5d0RKV09lMC03VENBbUpZWktFTXRrIn0.eyJleHAiOjE3MDU0Njk2ODYsImlhdCI6MTcwNTQ2MjQ4NiwiYXV0aF90aW1lIjoxNzA1NDYyNDg0LCJqdGkiOiI2NWVkMWY1Yy1jYzI5LTRkM2YtOTNkNC1hNDNiY2QxN2EyZDgiLCJpc3MiOiJodHRwczovL2tleWNsb2FrLnVhdC5qcC5jb2hlcmVudC5nbG9iYWwvYXV0aC9yZWFsbXMvbnR0ZGF0YSIsImF1ZCI6InByb2R1Y3QtZmFjdG9yeSIsInN1YiI6ImNlMWNlYzgxLWFjZDQtNGMwYi05NDNmLWRjMTE5Mzg2MTRiZiIsInR5cCI6IkJlYXJlciIsImF6cCI6InByb2R1Y3QtZmFjdG9yeSIsIm5vbmNlIjoiN2E3YWNlZWMtM2VhYi00MTJmLWJkNTUtMTdkYjM5NGJiNGEwIiwic2Vzc2lvbl9zdGF0ZSI6ImQzMzA3OGFiLTViMmUtNDQ2OC1iNjE2LTRlMzQ0ZTVmMDNlMiIsImFjciI6IjEiLCJhbGxvd2VkLW9yaWdpbnMiOlsiaHR0cHM6Ly9tb2RlbGluZy1jZW50ZXIuZGV2LmNvaGVyZW50Lmdsb2JhbCIsImh0dHBzOi8vbW9kZWxpbmctY2VudGVyLnVzLmNvaGVyZW50Lmdsb2JhbCIsImh0dHBzOi8vc2EudWF0LmpwLmNvaGVyZW50Lmdsb2JhbCIsImh0dHBzOi8vc2Euc3RhZ2luZy5jb2hlcmVudC5nbG9iYWwiLCJodHRwczovL3Byb2R1Y3RmYWN0b3J5LnVhdC5qcC5jb2hlcmVudC5nbG9iYWwiLCJodHRwczovL3NwYXJrLnVhdC5qcC5jb2hlcmVudC5nbG9iYWwiLCJodHRwczovL3NwYXJrLXVzZXItbWFuYWdlci51YXQuanAuY29oZXJlbnQuZ2xvYmFsIiwiaHR0cHM6Ly9tb2RlbGluZy1jZW50ZXIuc3RhZ2luZy5jb2hlcmVudC5nbG9iYWwiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIl19LCJzY29wZSI6Im9wZW5pZCBvZmZsaW5lX2FjY2VzcyBzcGFyayBwcm9maWxlIiwic2lkIjoiZDMzMDc4YWItNWIyZS00NDY4LWI2MTYtNGUzNDRlNWYwM2UyIiwibmFtZSI6Ik1hcmsgQmFuYXJpYSIsImdyb3VwcyI6WyJ1c2VyOnBmIl0sInJlYWxtIjoibnR0ZGF0YSIsInByZWZlcnJlZF91c2VybmFtZSI6Im1hcmsuYmFuYXJpYUBjb2hlcmVudC5nbG9iYWwiLCJnaXZlbl9uYW1lIjoiTWFyayIsImZhbWlseV9uYW1lIjoiQmFuYXJpYSIsInVzZXJfY3JlYXRlZF90aW1lc3RhbXAiOjE3MDUwNTI5NzkzNTMsInJlbGF0aW9uIjoicGFydG5lciJ9.dZLs9-Kl0gkX0ex07yap5yrFnasPnqGG4a0Q76u8r5_FDSZwb8OVGnU_JI4_F3xuwQYbFpF6LrE3AZLX0u2XnS1KhjeTa9OpMLsamerGKQwN6aU5gpQXf0UBpoXuyug-0GPv5PI37uuWcJzQKFYnz_s3bWUZ8J3IFsLO3l-ZbXxYtizfmU45Ix-j0GlhAmfLqT3LnoMm3eYr0Ej19-PrsUduhtuJhMqbQOwounUUmcaCEMUjt2sb_K-J9-p-9P8HZpIHaEqnjSn5TSIB5bBjUpFTlXEx7RE3Th_UxHx5YiksbQCR8GMyqe9_Ku9C_sZcPgPTC3dUDMUnPmaZNHhChA")
 
-    GenerateIncentiveReports_clicked = st.form_submit_button("Submit", use_container_width=True)
+    GenerateIncentiveReports_clicked = st.form_submit_button("## **Submit**", use_container_width=True)
     if GenerateIncentiveReports_clicked:
+      inputdata["EXEC_1"]["FileName"] = "Individual Sales Incentive Report - Exec " + inputdata["Exec1_Employee_ID"] + "- " + inputdata["Year"] + inputdata["Quarter"]
+      inputdata["ALL_EXECS"]["FileName"] = "All Execs - " + inputdata["Year"] + inputdata["Quarter"] + " - Sales Incentive Report"
       CallData = callSparkModel(inputdata)
       Outputs = CallData.json()['response_data']['outputs']
       SparkErrors = CallData.json()['response_data']['errors']
@@ -93,7 +97,9 @@ with col21:
         }
     </style>
     """, unsafe_allow_html=True)
-  st.write("Powered by Coherent")
+  image_path = "logotype-full-color.png"
+  st.image(image_path, caption="", width=210)
+  
 
 with col22:
   st.text("‎") 
@@ -101,22 +107,35 @@ with col22:
 with col23:
   st.text("‎")
 
-  st.write("Outputs")
+  st.write("**Outputs**")
   ERRORBOX = st.empty()
   with st.expander("Spark Model", expanded=True):
     st.markdown('[https://excel.uat.jp.coherent.global/nttdata/api/v3/folders/SIP/services/Sales%20Incentive%20Report%20Generator/Execute](https://excel.uat.jp.coherent.global/nttdata/api/v3/folders/SIP/services/Sales%20Incentive%20Report%20Generator/Execute)')
 
-  tab1, tab2 = st.tabs(["Individual Incentive By KPI", "All Execs Incentive By KPI"])
+  tab1, tab2 = st.tabs(["## **Individual**", "## **All Execs**"])
 
   with tab1:
 
     with st.expander("" , expanded=True):
-      col41, col42, col43 = st.columns([2,1,1])
+    #   col41, col42, col43, col44 = st.columns([1,4,1,1])
+    #   with col41:
+    #     EMPLOYEE = st.empty()
+    #   with col42:
+    #     ROLE = st.empty()
+    #   with col43:
+    #     INCENTIVE = st.empty()
+    #   with col44:
+    #     INCENTIVE_ADJ = st.empty()
+      col41, col42 = st.columns([4,1])
       with col41:
         EMPLOYEE = st.empty()
       with col42:
         INCENTIVE = st.empty()
+
+      col43, col44 = st.columns([4,1])
       with col43:
+        ROLE = st.empty()
+      with col44:
         INCENTIVE_ADJ = st.empty()
       st.markdown('***')
       st.write("**Breakdown By KPI**")
@@ -126,15 +145,33 @@ with col23:
       INDIV_REPORT = st.empty()
   
   with tab2:
-    with st.expander("**All Results**" , expanded=True):
-      col41, col42, col43, col44 = st.columns([1,1,1,1])
+    with st.expander("" , expanded=True):
+      # col41, col42, col43, col44, col45 = st.columns([1,2,2,1,2])
+      # with col41:
+      #   NUM_EMPLOYEES = st.empty()
+      # with col42:
+      #   TOTAL_INCENTIVES = st.empty()
+      # with col43:
+      #   BUDGET_PER_EXEC = st.empty()
+      # with col44:
+      #   ADJ_FACTOR = st.empty()
+      # with col45:
+      #   ADJUSTED_TOTAL = st.empty()
+
+      col41, col42, col43 = st.columns([1,1,1])
       with col41:
-        TOTAL_INCENTIVES = st.empty()
+        NUM_EMPLOYEES = st.empty()
       with col42:
         BUDGET_PER_EXEC = st.empty()
       with col43:
-        ADJ_FACTOR = st.empty()
+        TOTAL_INCENTIVES = st.empty()
+
+      col44, col45, col46 = st.columns([1,1,1])
       with col44:
+        AA = st.empty()
+      with col45:
+        ADJ_FACTOR = st.empty()
+      with col46:
         ADJUSTED_TOTAL = st.empty()
       st.markdown('***')
       st.write("**Breakdown By KPI**")
@@ -144,6 +181,8 @@ with col23:
       ALL_REPORT = st.empty()
 
 
+  inputdata["EXEC_1"]["FileName"] = "Individual_Sales_Incentive_Report_Exec_"+ inputdata["Exec1_Employee_ID"] + "_" + inputdata["Year"] + inputdata["Quarter"]
+  inputdata["ALL_EXECS"]["FileName"] = "All_Execs_" + inputdata["Year"] + inputdata["Quarter"] + "_Sales_Incentive_Report"
   CallData = callSparkModel(inputdata)
   Outputs = CallData.json()['response_data']['outputs']
   SparkErrors = CallData.json()['response_data']['errors']
@@ -176,19 +215,22 @@ with col23:
     INCENTIVE_ADJValue = "{:,.0f}".format(Outputs["Exec1_Incentive_Adj"])
     INCENTIVEValue = "{:,.0f}".format(Outputs["Exec1_Incentive_unAdj"])
 
-    EMPLOYEE.metric(label=ROLEValue, value=NAMEValue)
-    INCENTIVE.metric(label='Incentive', value=INCENTIVEValue)
+    EMPLOYEE.metric(label="Name", value=NAMEValue)
+    ROLE.metric(label="Role", value=ROLEValue)
+    INCENTIVE.metric(label='Incentive (Unadjusted)', value=INCENTIVEValue)
     INCENTIVE_ADJ.metric(label='Incentive (Adjusted)', value=INCENTIVE_ADJValue)
-    
+
+    NUM_EMPLOYEESValue = Outputs["numberOfExecutives"]
     TOTAL_INCENTIVESValue = "{:,.0f}".format(Outputs["All_Incentive_unAdj"])
     BUDGET_PER_EXECValue = "{:,.0f}".format(Outputs["Budget_perExec"])
     ADJ_FACTORValue = "{:,.2f}".format(Outputs["Adj_Factor"])
     ADJUSTED_TOTALValue = "{:,.0f}".format(Outputs["All_Incentive_Adj"])
     
-    TOTAL_INCENTIVES.metric(label="Total Incentives", value=TOTAL_INCENTIVESValue)
-    BUDGET_PER_EXEC.metric(label="Budget Per Executive", value=BUDGET_PER_EXECValue)
-    ADJ_FACTOR.metric(label="Adjustment Factor", value=ADJ_FACTORValue)
-    ADJUSTED_TOTAL.metric(label="Adjusted Total Incentives", value=ADJUSTED_TOTALValue)
+    NUM_EMPLOYEES.metric(label="Number of Executives", value=NUM_EMPLOYEESValue)
+    TOTAL_INCENTIVES.metric(label="Total Incentives (Unadjusted)", value=TOTAL_INCENTIVESValue)
+    BUDGET_PER_EXEC.metric(label="Budget (per executive)", value=BUDGET_PER_EXECValue)
+    ADJ_FACTOR.metric(label="Adjustment Factor (%)", value=ADJ_FACTORValue)
+    ADJUSTED_TOTAL.metric(label="Total Incentives (Adjusted)", value=ADJUSTED_TOTALValue)
 
     INDIV_REPORT.markdown(create_button_link(Outputs["EXEC_1"]["PDFUrl"], "Individual Incentive Report"), unsafe_allow_html=True)
     ALL_REPORT.markdown(create_button_link(Outputs["ALL_EXECS"]["PDFUrl"], "Full Incentive Report"), unsafe_allow_html=True)
